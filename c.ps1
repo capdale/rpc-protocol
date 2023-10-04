37b9bd31939b16979efd26896ca14bc4cbbe9b15
devhoodit <hooditgames@gmail.com>
2023-10-04 19:44:26 +0900

    rm

--- c.ps1
+++
@@ -1,8 +0,0 @@
-$proto_dir = "./proto"
-$gen_dir = "./gen"
-
-# Go
-protoc -I . --proto_path=. --go_out=$gen_dir/go/. --go_opt=paths=source_relative --go-grpc_out=$gen_dir/go/. --go-grpc_opt=paths=source_relative $proto_dir/*.proto
-
-# Python
-python -m grpc_tools.protoc -I . --python_out=$gen_dir/python --grpc_python_out=$gen_dir/python --pyi_out=$gen_dir/python $proto_dir/*.proto
